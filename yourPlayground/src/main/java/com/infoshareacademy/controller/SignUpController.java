b054c0a9bc9aa844be30a0fb32979e15b3e3fc4e
Jakub Rogowski <jakerogowski33@gmail.com>
2022-09-04 12:32:15 +0200

    showRegistrationForm method in controller added

--- yourPlayground/src/main/java/com/infoshareacademy/controller/SignUpController.java
+++ yourPlayground/src/main/java/com/infoshareacademy/controller/SignUpController.java
@@ -1,13 +1,18 @@
 package com.infoshareacademy.controller;
 
+import com.infoshareacademy.dto.PlayerDto;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.context.request.WebRequest;
 
 @Controller
 public class SignUpController {
 
     @GetMapping("/registration")
-    public String signIn() {
-        return "sign-in";
+    public String showRegistrationForm(WebRequest request, Model model) {
+        PlayerDto userDto = new PlayerDto();
+        model.addAttribute("user", userDto);
+        return "registration";
     }
 }
