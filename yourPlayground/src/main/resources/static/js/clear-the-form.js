a4dc389e4bc96813c7785ed6ffe493b1676ee841
Jakub Rogowski <jakerogowski33@gmail.com>
2022-10-07 13:09:50 +0200

    Extracted js scripts, getting values from enum added

---
+++ yourPlayground/src/main/resources/static/js/clear-the-form.js
@@ -0,0 +1,11 @@
+let nameInput = document.getElementById("name_input_id");
+let typeInput = document.getElementById("type_input_id");
+let townInput = document.getElementById("town_input_id");
+let dateInput = document.getElementById("date_input_id");
+
+function resetForm() {
+    nameInput.value = "";
+    typeInput.value = "";
+    townInput.value = "";
+    dateInput.value = "";
+}
